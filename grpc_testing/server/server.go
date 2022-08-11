c16f0037f7cb89811485e439e1db0d79112f8711
datnd <datnd@hybrid-technologies.vn>
2022-08-11 14:07:55 +0700

    idk anything

---
+++ grpc_testing/server/server.go
@@ -0,0 +1,171 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"log"
+	"math"
+	"net"
+	"time"
+
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
+	calculatorpb "learn_grpc_datnd/learn_grpc_datnd/calculator"
+
+	"google.golang.org/grpc"
+)
+
+type server struct {
+	calculatorpb.UnimplementedCalculatorServiceServer
+}
+
+// mustEmbedUnimplementedCalculatorServiceServer implements calculator.CalculatorServiceServer
+func (*server) mustEmbedUnimplementedCalculatorServiceServer() {
+	panic("unimplemented")
+}
+
+func (*server) Sum(ctx context.Context, req *calculatorpb.SumRequest) (*calculatorpb.SumResponse, error) {
+	log.Println("sum called...")
+	resp := &calculatorpb.SumResponse{
+		Result: req.GetNum1() + req.GetNum2(),
+	}
+
+	return resp, nil
+}
+
+func (*server) SumWithDeadline(ctx context.Context, req *calculatorpb.SumRequest) (*calculatorpb.SumResponse, error) {
+	log.Println("sum with deadline called...")
+
+	for i := 0; i < 3; i++ {
+		if ctx.Err() == context.Canceled {
+			log.Println("context.Canceled...")
+			return nil, status.Error(codes.Canceled, "client canceled req")
+		}
+		time.Sleep(1 * time.Second)
+	}
+
+	resp := &calculatorpb.SumResponse{
+		Result: req.GetNum1() + req.GetNum2(),
+	}
+
+	return resp, nil
+}
+
+func (*server) PrimeNumberDecomposition(req *calculatorpb.PNDRequest,
+	stream calculatorpb.CalculatorService_PrimeNumberDecompositionServer) error {
+	log.Println("PrimeNumberDecomposition called...")
+	k := int32(2)
+	N := req.GetNumber()
+	for N > 1 {
+		if N%k == 0 {
+			N = N / k
+			//send to client
+			stream.Send(&calculatorpb.PNDResponse{
+				Result: k,
+			})
+			time.Sleep(1000 * time.Millisecond)
+		} else {
+			k++
+			log.Printf("k increase to %v", k)
+		}
+	}
+	return nil
+}
+
+func (*server) Average(stream calculatorpb.CalculatorService_AverageServer) error {
+	log.Println("Average called..")
+	var total float32
+	var count int
+	for {
+		req, err := stream.Recv()
+		if err == io.EOF {
+			//tinh trung binh va return cho client
+			resp := &calculatorpb.AverageResponse{
+				Result: total / float32(count),
+			}
+
+			return stream.SendAndClose(resp)
+		}
+		if err != nil {
+			log.Fatalf("err while Recv Average %v", err)
+			return err
+		}
+		log.Printf("receive req %v", req)
+		total += req.GetNum()
+		count++
+	}
+}
+
+func (*server) FindMax(stream calculatorpb.CalculatorService_FindMaxServer) error {
+	log.Println("Find max called ....")
+	max := int32(0)
+	for {
+		req, err := stream.Recv()
+		if err == io.EOF {
+			log.Println("EOF...")
+			return nil
+		}
+		if err != nil {
+			log.Fatalf("err while Recv FindMax %v", err)
+			return err
+		}
+
+		num := req.GetNum()
+		log.Printf("recv num %v\n", num)
+		if num > max {
+			max = num
+		}
+		err = stream.Send(&calculatorpb.FindMaxResponse{
+			Max: max,
+		})
+		if err != nil {
+			log.Fatalf("send max err %v", err)
+			return err
+		}
+	}
+}
+
+func (*server) Square(ctx context.Context, req *calculatorpb.SquareRequest) (*calculatorpb.SquareResponse, error) {
+	log.Println("Square called...")
+	num := req.GetNum()
+	if num < 0 {
+		log.Printf("req num < 0, num=%v, return InvalidArgument", num)
+		return nil, status.Errorf(codes.InvalidArgument, "Expect num > 0, req num was %v", num)
+	}
+
+	return &calculatorpb.SquareResponse{
+		SquareRoot: math.Sqrt(float64(num)),
+	}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", "0.0.0.0:50069")
+	if err != nil {
+		log.Fatalf("err while create listen %v", err)
+	}
+	s := grpc.NewServer()
+
+	calculatorpb.RegisterCalculatorServiceServer(s, &server{})
+	//calculatorpb.RegisterCalculatorServiceServer(s, &server{})
+
+	fmt.Println("calculator is running...")
+	err = s.Serve(lis)
+
+	if err != nil {
+		log.Fatalf("err while serve %v", err)
+	}
+}
+
+// certFile := "ssl/server.crt"
+// keyFile := "ssl/server.key"
+
+// creds, sslErr := credentials.NewServerTLSFromFile(certFile, keyFile)
+// if sslErr != nil {
+// 	log.Fatalf("create creds ssl err %v\n", sslErr)
+// 	return
+// }
+// opts := grpc.Creds(creds)
+
+// s := grpc.NewServer(opts)
